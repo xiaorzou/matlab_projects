80030f69526f03ffd879ec8e0d8ec4c53522ed66
xiaorzou <41243320+xiaorzou@users.noreply.github.com>
2025-11-29 16:53:52 -0500

    Delete src directory

--- src/performance_cut_off_para/mylib_writearray.m
+++
@@ -1,8 +0,0 @@
-function mylib_writearray(vnames, input_array, filename_full)
-    input_array = array2table(input_array, 'VariableNames',vnames);
-    if exist(filename_full, 'file')
-        delete(filename_full);
-    end
-    writetable(input_array,filename_full)
-end
-
